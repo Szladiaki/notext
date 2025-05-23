893648ec9068c4ff0ae9e16e8b878de8df0fb196
Szladiaki <szladicsek.akos@bottyan.eu>
2025-05-23 10:37:47 +0200

    docker fix

--- frontend/app.js
+++ frontend/app.js
@@ -4,7 +4,7 @@ let currentUser = localStorage.getItem("user") || "";
 let isLogin = true;
 let editingNoteId = null;
 
-const API_BASE = "http://localhost:3001";
+const API_BASE = "";
 
 document.addEventListener("DOMContentLoaded", () => {
   if (token && currentUser) {
